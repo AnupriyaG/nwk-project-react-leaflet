2d5e87721aba53440ba4c3a36aeec3c5c5c482d5
Anupriya <agotkhindi4@gmail.com>
2021-04-08 22:35:54 -0400

    Adding newark project to this repo

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Router , Switch,Link} from 'react-router-dom';
-//import 'leaflet/dist/leaflet.css';
-
+//import 'leaflet/dist/leaflet.css'
 //import 'normalize.css';
-
 import Header from './components/Header';
 import Home from './components/Home';
 import ReactLeafletMap from './components/ReactLeaflet';
+
 import './styles/main.styl';
 
 function App() {
